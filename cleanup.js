63925df0e12ffc56cc3a15073ccf2a9873293d4f
Cal Loomis <charles.loomis@relativity.com>
2021-02-07 12:58:50 -0600

    remove main script

--- cleanup.js
+++ cleanup.js
@@ -1,7 +1,15 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 
 const finalize = core.getInput('finalize')
 console.log("finalize: ", finalize);
 
-const state = core.getState("state");
-console.log("state: ", state);
+const jobStart = core.getState("jobStart");
+const jobStop = Date.now();
+const delta = jobStop - jobStart;
+console.log(`job timing: ${jobStart}, ${jobStop}, ${delta}`);
+
+// Get the JSON webhook payload for the event that triggered the workflow
+const payload = JSON.stringify(github.context.payload, undefined, 2)
+console.log(`event payload:\n${payload}`);
+
--- index.js
+++
@@ -1,5 +0,0 @@
-const github = require('@actions/github');
-
-// Get the JSON webhook payload for the event that triggered the workflow
-const payload = JSON.stringify(github.context.payload, undefined, 2)
-console.log(`The event payload: ${payload}`);
